820f71665a6ee35f56111134d25e7ad065403687
rusins <raitis.krikis@gmail.com>
2023-06-03 21:19:06 +0300

    delete node modules

--- src/data-fetching/student-count.ts
+++ src/data-fetching/student-count.ts
@@ -6,7 +6,7 @@ import * as https from 'https';
 import * as fs from 'fs';
 
 const studentCountUrl = "https://data.gov.lv/dati/dataset/9c8954d6-d8b7-4ebd-97cb-219b3bbc75d9/resource/fe40a325-e4d1-465e-a6e1-a63d675cc71b/download/izglitojamoskvispizglprogr_pa_iestadem_01122022.xlsx";
-
+const skoluKarteUrl = "https://izm.kartes.lv/api?action=layer&layer=skolas_2018.geojson";
 
 function downloadFile(filePath: string, url: string): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -25,18 +25,27 @@ function downloadFile(filePath: string, url: string): Promise<void> {
 };
 
 tmp.file((err, filePath, fd, cleanupCallback) => {
-  if (err) throw (err);
-  downloadFile(filePath, studentCountUrl).then(() => {
-    const workbook = xlsx.readFile(filePath);
-    const sheetNames = workbook.SheetNames;
+  return new Promise<void>((resolve, reject) => {
+    if (err) {
+      reject(err);
+    } else {
+      downloadFile(filePath, studentCountUrl).then(() => {
+        const workbook = xlsx.readFile(filePath);
+        const sheetNames = workbook.SheetNames;
 
-    // Get the data of "Sheet1"
-    const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetNames[0]])
+        // Get the data of "Sheet1"
+        const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetNames[0]])
 
-    // Do what you need with the received data
-    data.map(row => {
-      console.log(data[0]);
-    });
+        // Do what you need with the received data
+        data.map(row => {
+          console.log(data[0]);
+        });
 
-  }).catch((err) => { throw (err) });
+      }).catch((err) => { reject(err) });
+    }
+  });
+});
+
+https.get(skoluKarteUrl, (res) => {
+  console.log(res);
 });
