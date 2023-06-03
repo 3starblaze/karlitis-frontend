1450aee936c52837f795a440baae2a4c730c8fd6
3starblaze <3starblaze@protonmail.com>
2023-06-03 22:16:28 +0300

    Restore app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import './App.css';
+import Dropdown from  './Dropdown';
+import Map from './Map';
+import * as L from 'leaflet';
+import '/node_modules/leaflet/dist/leaflet.css';
 
 function App() {
   return (
-    <div className="bg-gray-100">
-      <p className="p-20">
-        Test paragraph.
-      </p>
+    <div className="bg-gray-100 h-screen">
+      <header className="bg-white shadow p-4">
+        <p className="text-2xl">Kārlītis</p>
+      </header>
+
+
+      <div className="flex flex-col lg:flex-row-reverse">
+
+        <Map />
+
+        <div className="lg:w-80 lg:h-screen">
+          <Dropdown title="Rīgas Pilsētas skola">
+            <p>Eksāmena vidējā vērtība:</p>
+            <p className="pl-4">9.klase 89.92%</p>
+            <p className="pl-4">12.klase 92.73%</p>
+          </Dropdown>
+        </div>
+      </div>
     </div>
   );
 }
