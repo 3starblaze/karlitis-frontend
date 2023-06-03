f5f8a9325da8403140a072d97fb9b5ac8acf68cf
towergame <towergame.epic@gmail.com>
2023-06-03 20:55:20 +0300

    map fix

--- src/Map.js
+++ src/Map.js
@@ -2,14 +2,16 @@ import { useEffect } from 'react';
 import * as L from 'leaflet';
 import '/node_modules/leaflet/dist/leaflet.css';
 
+let map;
 function Map() {
     useEffect(() => {
-        let map = L.map('map').setView([51.505, -0.09], 13);
+        if(!map) map = L.map('map').setView([51.505, -0.09], 13);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        let test = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+        });
+        test.addTo(map);
     });
     return (
         <div
