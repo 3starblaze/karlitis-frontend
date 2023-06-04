e59f9364c9714bdf0eabce9027ba902b5960870a
Scarpy019 <Scarpy019@gmail.com>
2023-06-04 11:56:56 +0300

    models set up

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { sequelize } from './sequelize_setup.js';
+import { StudentCount } from './models/student_count.js';
 const app = express();
 
 app.get('/reklama', (req, res) => {
@@ -10,4 +11,5 @@ app.use('/', express.static('./build'));
 console.log("cau");
 console.log("Started at 3000");
 app.listen(3000); 
-sequelize.sync();
+sequelize.sync({alter: false, force: false});
+StudentCount.findAll({where: { count_1_klase: 4 }});
