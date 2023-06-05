3b8b6a5903b0d4efce49355876d357b4008b8b06
rusins <raitis.krikis@gmail.com>
2023-06-05 03:37:39 +0300

    Merge fuzzy search

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Map from './Map';
-import { Menu, Select, Slider } from 'antd';
+import { Menu, Select, Slider, Input } from 'antd';
 import * as L from 'leaflet';
 import { useEffect, useState } from 'react';
 import '/node_modules/leaflet/dist/leaflet.css';
 import ExamForm from './ExamForm';
 import proj4 from 'proj4';
 import RangeSlider from './Slider';
+import Fuse from 'fuse.js';
 
 function baseMenuItems() {
   const children = [
@@ -64,19 +65,16 @@ function schoolToPoint(school: any) {
   };
 }
 
-function getURLParameter(sParamL: string)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] === sParamL) 
-        {
-            return sParameterName[1];
-        }
+function getURLParameter(sParamL: string) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] === sParamL) {
+      return sParameterName[1];
     }
-	return null;
+  }
+  return null;
 }
 
 function App() {
@@ -84,25 +82,36 @@ function App() {
   const [schools, setSchools] = useState<any[]>([]);
   const [home, setHome] = useState<L.LatLng | null>(null);
   const [maxDist, setMaxDist] = useState<number>(50_000);
+  const fuseOptions = {
+    keys: ['name']
+  };
+  const [fuse, setFuse] = useState<Fuse<any[]> | null>(null);
 
+  function onSearch(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) {
+    let results = schoolsRaw;
+    if (e.target.value !== "")
+      results = fuse!.search(e.target.value).map(res => res.item);
+
+    setSchools(results);
+  };
 
   function checkDistance(school: any) {
-	  const p = schoolToPoint(school);
-	  const latLngCoord = proj4("EPSG:3857", "EPSG:4326", p.latLng);
-	  const flippedCoord: [number, number] = [latLngCoord[1], latLngCoord[0]];
-	  const flippedLatLng = L.latLng(flippedCoord);
+    const p = schoolToPoint(school);
+    const latLngCoord = proj4("EPSG:3857", "EPSG:4326", p.latLng);
+    const flippedCoord: [number, number] = [latLngCoord[1], latLngCoord[0]];
+    const flippedLatLng = L.latLng(flippedCoord);
 
-	  return flippedLatLng.distanceTo(home!) < maxDist;
-	}
+    return flippedLatLng.distanceTo(home!) < maxDist;
+  }
 
-	function getDistance(school: any) {
-	  const p = schoolToPoint(school);
-	  const latLngCoord = proj4("EPSG:3857", "EPSG:4326", p.latLng);
-	  const flippedCoord: [number, number] = [latLngCoord[1], latLngCoord[0]];
-	  const flippedLatLng = L.latLng(flippedCoord);
+  function getDistance(school: any) {
+    const p = schoolToPoint(school);
+    const latLngCoord = proj4("EPSG:3857", "EPSG:4326", p.latLng);
+    const flippedCoord: [number, number] = [latLngCoord[1], latLngCoord[0]];
+    const flippedLatLng = L.latLng(flippedCoord);
 
-	  return flippedLatLng.distanceTo(home!);
-	}
+    return flippedLatLng.distanceTo(home!);
+  }
 
   useEffect(() => {
     if (schools?.length !== 0) return;
@@ -112,36 +121,37 @@ function App() {
     //         // console.log(data);
     //       });
 
-		  let formData: {address: string | null, checkedExams: string[]} = {
-				address: getURLParameter("address"),
-				checkedExams: []
-			}
+    let formData: { address: string | null, checkedExams: string[] } = {
+      address: getURLParameter("address"),
+      checkedExams: []
+    }
 
-			if(getURLParameter("matematika") !== null) formData.checkedExams.push("matematika");
-			if(getURLParameter("latv_val") !== null) formData.checkedExams.push("latv_val");
-			if(getURLParameter("anglu_val") !== null) formData.checkedExams.push("anglu_val");
-			if(getURLParameter("francu_val") !== null) formData.checkedExams.push("francu_val");
-			if(getURLParameter("krievu_val") !== null) formData.checkedExams.push("krievu_val");
-			if(getURLParameter("vacu_val") !== null) formData.checkedExams.push("vacu_val");
-			if(getURLParameter("biologija") !== null) formData.checkedExams.push("biologija");
-			if(getURLParameter("fizika") !== null) formData.checkedExams.push("fizika");
-			if(getURLParameter("kimija") !== null) formData.checkedExams.push("kimija");
-			if(getURLParameter("vesture") !== null) formData.checkedExams.push("vesture");
+    if (getURLParameter("matematika") !== null) formData.checkedExams.push("matematika");
+    if (getURLParameter("latv_val") !== null) formData.checkedExams.push("latv_val");
+    if (getURLParameter("anglu_val") !== null) formData.checkedExams.push("anglu_val");
+    if (getURLParameter("francu_val") !== null) formData.checkedExams.push("francu_val");
+    if (getURLParameter("krievu_val") !== null) formData.checkedExams.push("krievu_val");
+    if (getURLParameter("vacu_val") !== null) formData.checkedExams.push("vacu_val");
+    if (getURLParameter("biologija") !== null) formData.checkedExams.push("biologija");
+    if (getURLParameter("fizika") !== null) formData.checkedExams.push("fizika");
+    if (getURLParameter("kimija") !== null) formData.checkedExams.push("kimija");
+    if (getURLParameter("vesture") !== null) formData.checkedExams.push("vesture");
 
-			const response = fetch("http://localhost:6942/api/schools/nearby", {
-					method: 'POST',
-					headers: new Headers({'content-type': 'application/json'}),
-					body: JSON.stringify(formData)
-				})
-				.then((response) => response.json())
-				.then((data) => {
-					if(getURLParameter("address") !== "") setHome(data.homeLatLang === null ? null : L.latLng([parseFloat(data.homeLatLng[0]), parseFloat(data.homeLatLng[1])]));
-					setSchoolsRaw(data.updatedSchoolList);
-				});
+    const response = fetch("http://localhost:6942/api/schools/nearby", {
+      method: 'POST',
+      headers: new Headers({ 'content-type': 'application/json' }),
+      body: JSON.stringify(formData)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (getURLParameter("address") !== "") setHome(data.homeLatLang === null ? null : L.latLng([parseFloat(data.homeLatLng[0]), parseFloat(data.homeLatLng[1])]));
+        setSchoolsRaw(data.updatedSchoolList);
+        setFuse(new Fuse(data, fuseOptions));
+      });
   }, []);
 
   useEffect(() => {
-	setSchools(schoolsRaw.filter((val) => home === null ? true : checkDistance(val)).sort((a, b) => home === null ? a.examScore - b.examScore : getDistance(a) - getDistance(b)))
+    setSchools(schoolsRaw.filter((val) => home === null ? true : checkDistance(val)).sort((a, b) => home === null ? a.examScore - b.examScore : getDistance(a) - getDistance(b)))
   }, [schoolsRaw, home, maxDist]);
 
   return (
@@ -170,7 +180,7 @@ function App() {
         <Menu
           className="lg:hidden"
           mode="vertical"
-          items={ menuItems() }
+          items={menuItems()}
         />
 
       </header>
@@ -178,66 +188,33 @@ function App() {
 
       <div className="flex flex-col lg:flex-row-reverse">
         <Map
-          points={ schools.map(schoolToPoint) }
-		  home={ home }
+          points={schools.map(schoolToPoint)}
+          home={home}
         />
-        <div className="flex flex-col lg:w-2/3 lg:h-screen overflow-scroll">
-          <div className="fixed bg-custom-white w-full p-4 z-50 border-b border-custom-blue shadow-md">
-            { schools?.length } rezultāti
+        <div className="flex flex-col lg:w-2/3 lg:h-screen">
+          <div className="sticky bg-custom-white w-full p-4 z-50 border-b border-custom-blue shadow-md">
+            <Input placeholder="meklēt" onChange={onSearch} style={{ width: 200 }} /> {schools?.length} rezultāti
+            <div>
+              <p className="mb-2">
+                Maksimālais attālums
+              </p>
+
+              <RangeSlider value={maxDist} callback={(val) => setMaxDist(val)} />
+            </div>
           </div>
 
-          {/* Non-fixed element duplicate is here in order to reserve sapce */}
-          <div className="w-full p-4">
-            { schools?.length } rezultāti
-          </div>
 
 
-          {/* Card */}
-          <div className="m-4 p-4 border border-custom-blue shadow-md">
-            <p>Hello there</p>
-            <p>This is an example paragraph.</p>
-          </div>
-
-		  <div className="m-4 p-4 border border-custom-blue shadow-md">
-		  	{/* <ExamForm onSubmit={(formData) => {
-				const response = fetch("http://localhost:6942/api/schools/nearby", {
-					method: 'POST',
-					headers: new Headers({'content-type': 'application/json'}),
-					body: JSON.stringify(formData)
-				})
-				.then((response) => response.json())
-				.then((data) => {
-					setHome(data.homeLatLang === null ? null : L.latLng([parseFloat(data.homeLatLng[0]), parseFloat(data.homeLatLng[1])]));
-					setSchoolsRaw(data.updatedSchoolList);
-				});
-			}}/> */}
-          </div>
-
-          <div className="m-4 p-4 border border-custom-blue shadow-md flex flex-col">
-            <p className="mb-2">
-              Izvēlies skolu
-            </p>
-            <Select
-              options={ schoolSelectOptions() }
-            />
-          </div>
-
-          <div>
-			 <p className="mb-2">
-              Maksimālais attālums
-            </p>
-
-			<RangeSlider value={maxDist} callback={(val) => setMaxDist(val)}/>
-			
+          <div className="overflow-scroll">
             <ul>
-              { schools?.map((val) => (
+              {schools?.map((val) => (
                 <li
-                  key={ val.id }
+                  key={val.id}
                   className="m-4 border border-custom-blue rounded-md shadow-md flex flex-col"
                 >
-                  <div className="border-b border-custom-blue p-4 text-xl">{ val.name }</div>
+                  <div className="border-b border-custom-blue p-4 text-xl">{val.name}</div>
                   <p className="p-4">
-                    Centralā eksāmena rezultāts: <span className="text-xl">{ val.examScore }%</span>
+                    Centralā eksāmena rezultāts: <span className="text-xl">{val.examScore}%</span>
                   </p>
 
                 </li>
